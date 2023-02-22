adad3c6c3ae1afc8d1338c0d2a1f50fc9553e1d0
codethrx <tatheermehdi2000@gmail.com>
2023-02-22 23:20:02 +0500

    NextAuth added.

---
+++ app/layout.js
@@ -0,0 +1,24 @@
+import "./globals.css";
+import Session from "./components/Session";
+import { getServerSession } from "next-auth/next";
+import { authOptions } from "@/pages/api/auth/[...nextauth]";
+import Header from "./components/Header";
+export default async function RootLayout({ children }) {
+  const session = await getServerSession(authOptions);
+
+  return (
+    <html lang="en">
+      {/*
+        <head /> will contain the components returned by the nearest parent
+        head.js. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
+      */}
+      <head />
+      <body>
+        <Session session={session}>
+          <Header />
+          {children}
+        </Session>
+      </body>
+    </html>
+  );
+}
---
+++ pages/api/auth/[...nextauth].js
@@ -0,0 +1,20 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@next-auth/prisma-adapter";
+import GithubProvider from "next-auth/providers/github";
+import GoogleProvider from "next-auth/providers/google";
+// import prisma from "../../../libs/prisma";
+
+export const authOptions = {
+  // Configure one or more authentication providers
+  //   adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_ID,
+      clientSecret: process.env.GOOGLE_SECRET,
+    }),
+
+    // ...add more providers here
+  ],
+};
+
+export default NextAuth(authOptions);
